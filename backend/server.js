9f3276ab35f94fffdadb2f3131b7a8dc8948e583
Joel Corbalán <141983168+Joel210211@users.noreply.github.com>
2024-10-22 17:37:34 -0300

    servidor
    agrego archivo servidor.js

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+//Conexión 
+mongoose.connect('mongodb://localhost:27017/techconnet')
+
+.then(() => {
+    console.log('Conectado a MongoDB');
+})
+.catch (err => console.error('Error al conectar a MongoDB', err));
+
+//posibles rutas
+app.use('/categoria', categoriaRutas);
+app.use('/producto', productoRutas);
+app.use('/compra', compraRutas);
+app.use('/cliente', clienteRutas);
+
+//iniciar servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
